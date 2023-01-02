2c2076291bcafbbef64c1f2ab7545b8ab8cf192f
Daniel Mera <danielfernansdo404@gmail.com>
2023-01-02 13:46:50 -0500

    agregando-espaciados

--- carpeta-trabajo-deber/carpeta-trabajo/archivo1.cpp
+++ carpeta-trabajo-deber/carpeta-trabajo/archivo1.cpp
@@ -5,6 +5,7 @@ using namespace std;
     double suma(double x, double y);
     double resta(double x, double y);
     double multiplicacion(double x, double y);
+    double division(double x, double y);
 
 int main(){
     double numero1, numero2;
@@ -16,20 +17,28 @@ int main(){
         cin>>numero1;
         cout<<"digite otro numero: "<<endl;
         cin>>numero2;
-        cout<<"que operacion desea hacer: 1-para sumar, 2-para restar, 3-para multiplicar"<<endl;
+        cout<<"que operacion desea hacer: 1-para sumar, 2-para restar, 3-para multiplicar, 4-para dividir"<<endl;
         cin>>opcion;
         if(opcion == 1){
             cout<<"la suma es: "<<suma(numero1,numero2)<<endl;
+            cout<<endl;
         }
         if(opcion == 2){
             cout<<"la resta es: "<<resta(numero1,numero2)<<endl;
+            cout<<endl;
         }
         if(opcion == 3){
             cout<<"la multiplicacion es: "<<multiplicacion(numero1,numero2)<<endl;
+            cout<<endl;
+        }
+        if(opcion == 4){
+            cout<<"la division es: "<<division(numero1,numero2)<<endl;
+            cout<<endl;
         }
         
-        cout<<"para salir del sistema presione 1, caso contrario presione otro numero";
+        cout<<"para salir del sistema presione 0, caso contrario presione otro numero";
         cin>>salida;
+        cout<<endl;
     }
 
     
@@ -51,4 +60,9 @@ double multiplicacion(double x, double y){
     double producto;
     producto = x+y;
     return producto;
+}
+double division(double x, double y){
+    double inveroMultiplicativo;
+    inveroMultiplicativo = x/y;
+    return inveroMultiplicativo;
 }
